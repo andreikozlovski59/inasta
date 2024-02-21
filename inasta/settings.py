73d7a09bcf975a22da751133d9d1dbd58098905e
andreikozlovski59 <143131370+andreikozlovski59@users.noreply.github.com>
2024-02-21 20:49:44 +0300

    Update settings.py

--- inasta/settings.py
+++ inasta/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2&3ki&azz*5-xb04l*@g4kzk+^b=2z1*hj*@2h18k*n6s!-xmu'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
